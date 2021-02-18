6b0ebb28376b021669f6ae0c553b57f2dabaec5b
EnduranceCode <endurancecode@gmail.com>
2021-02-18 23:16:23 +0000

    V02: Add todos

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,14 +1,66 @@
 import {LitElement, html} from '@polymer/lit-element';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
+
+const VisibilityFilters = {
+    SHOW_ALL: 'All',
+    SHOW_ACTIVE: 'Active',
+    SHOW_COMPLETED: 'Completed'
+}
 
 class TodoView extends LitElement {
 
+    static get properties() {
+        return {
+            todos: { type: Array},
+            filter: { type: String},
+            task: { type: String}
+        }
+    }
+
+    constructor() {
+        super();
+        this.todos = [];
+        this.filter = VisibilityFilters.SHOW_ALL;
+        this.task = '';
+    }
+
     render() {
         return html `
-            <p>Hello World!</p>
+            <div class="input-layout" @keyup="${this.shortcutListner}">
+                <vaadin-text-field
+                    placeholder="Task"
+                    value="${this.task}"
+                    @change="${this.updateTask}">
+                </vaadin-text-field>
+                <vaadin-button theme="primary" @click="${this.addTodo}">Add Todo</vaadin-button>
+            </div>
         `;
     }
 
+    updateTask(e) {
+        this.task = e.target.value;
+    }
 
+    shortcutListner(e) {
+        if (e.key == 'Enter') {
+            this.addTodo();
+        }
+    }
+
+    addTodo() {
+        if(this.task) {
+            this.todos = [... this.todos, {
+                task: this.task,
+                complete: false
+            }];
+        }
+
+        this.task = '';
+    }
 }
 
 customElements.define('todo-view', TodoView);
