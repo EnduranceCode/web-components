2280cbfed61a95d277fe3d245ae3eb7ecca8bdd7
Ricardo do Canto <endurancecode@gmail.com>
2021-03-08 17:22:49 +0000

    COD: [WCCC] Add markup and style to the Custom Element

--- transversy-media-crash-course/userCard.js
+++ transversy-media-crash-course/userCard.js
@@ -1,14 +1,41 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
-        h3 {
-            color: coral;
+        .user-card {
+            font-family: 'Arial', sans-serif;
+            background: #f4f4f4;
+            width: 500px;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-gap: 10px;
+            margin-bottom: 15px;
+            border-bottom: #ff4081 5px solid;
+        }
+        
+        .user-card img {
+            width: 100%;
+        }
+        
+        .user-card button {
+            cursor: pointer;
+            background: #ff4081;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            padding: 5px 10px;
         }
     </style>
 
-    <div>
+    <div class="user-card">
         <img/>
-        <h3></h3>
+        <div>
+            <h3></h3>
+            <div class="info">
+                <p>EMAIL</p>
+                <p>PHONE</p>
+            </div>
+            <button id="toggle-info" >Hide Info</button>
+        </div>
     </div>
 `;
 
